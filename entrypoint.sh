376fbca550ab20268aba08f20645088e2bfe1882
Jeff Harris <jeff@1amstudios.com>
2025-08-21 12:29:13 -0700

    paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,6 @@ wine cmake -B $BUILDDIR . $INPUT_CMAKE_FLAGS
 # Build
 wine cmake --build $BUILDDIR -- -j1
 
-echo build_only: $INPUT_BUILD_ONLY
 if [ "${INPUT_BUILD_ONLY}" = "true" ]; then
   exit 0
 fi
@@ -22,8 +21,7 @@ curl -Lo /tmp/$INPUT_ORIGINAL_BINARY_FILENAME $INPUT_ORIGINAL_BINARY_URL
 reccmp-project detect --search-path /tmp
 
 # Fix up wine paths to underlying linux paths so we can run reccmp outside of wine
-cd $BUILDDIR
-sed -i 's/Z://g' reccmp-build.yml
+sed -i 's/Z://g' $BUILDDIR/reccmp-build.yml
 
 found=0
 while IFS= read -r line; do
@@ -31,13 +29,8 @@ while IFS= read -r line; do
   if [[ "$line" == *Decreased* ]]; then
     found=1
   fi
-done < <(reccmp-reccmp --target $INPUT_TARGET --html reccmp-report.html --json reccmp-report-new.json --diff $INPUT_REPORT_FILENAME)
+done < <(reccmp-reccmp --target $INPUT_TARGET --html reccmp-report.html --json $INPUT_REPORT_FILENAME --diff $INPUT_DIFF_REPORT_FILENAME)
 if (( found )); then
-  exit 1
-fi
-
-# Check if "Decreased" appears anywhere
-if grep -q "Decreased" <<< "$DIFF_OUTPUT"; then
   exit 1
 fi
 
